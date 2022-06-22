4c754e3b594ea372463b443e9ac6a9e928307048
koheiYamamoto <77714237+yamamoto-numblab@users.noreply.github.com>
2022-06-23 01:30:42 +0900

    Update GUI_test.py

--- GUI_test.py
+++ GUI_test.py
@@ -4,14 +4,23 @@ import datetime
 #被験者のNo
 subject = "sub1"
 
-dt_now = datetime.datetime.now()
-print(dt_now.strftime('%Y_%m%d')+subject)
+def subDay():
+    dt_now = datetime.datetime.now()
+    print(dt_now.strftime('%Y_%m%d')+subject)
+    return(dt_now.strftime('%Y_%m%d')+subject)
+
+
+
+def disp():
+    label = tk.Label(root, text='+',font=("", 150),foreground='#ff0000')#center_markerの表示
+    label.pack(anchor='center',expand=1)
+
 
 root = tk.Tk()
 root.title('test')#ウインドウネーム
 root.state('zoomed')#GUI画面の最大化
 
-label = tk.Label(root, text='+',font=("", 150),foreground='#ff0000')#center_markerの表示
-label.pack(anchor='center',expand=1)
+disp()
+subDay()
 
 root.mainloop()
