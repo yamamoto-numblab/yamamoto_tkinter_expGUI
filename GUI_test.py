07844544d2679b996b92ea239427f4da1c96e143
koheiYamamoto <77714237+yamamoto-numblab@users.noreply.github.com>
2022-06-23 14:17:58 +0900

    被験者提示画面

--- GUI_test.py
+++ GUI_test.py
@@ -7,16 +7,22 @@ subject = "sub1"
 def subDay():
     dt_now = datetime.datetime.now()
     print(dt_now.strftime('%Y_%m%d')+subject)
-    
+
 #実行時間の計測用
 start = time.time()
 
 #実験の時間計測
 root = tk.Tk()
 root.title('test')#ウインドウネーム
-root.state('zoomed')#GUI画面の最大化
+#root.state('zoomed')#GUI画面の最大化
+a = root.winfo_screenwidth()
+b = root.winfo_screenheight()
+root.geometry(str(a)+"x"+str(b))
 #center(+)の表示
-label = tk.Label(root, text='+',font=("", 150),foreground='#ff0000')#center_markerの表示
-label.pack(anchor='center',expand=1)
+center = tk.Label(root, text='+',font=("", 150),foreground='#ff0000').place(x = a/2-50, y = b/2-150)
+left = tk.Label(root, text='<',font=("", 150),foreground='#000000').place(x = a/2-200, y = b/2-150)
+right = tk.Label(root, text='>',font=("", 150),foreground='#000000').place(x = a/2+100, y = b/2-150)
+
+#center_markerの表示
 
 root.mainloop()
